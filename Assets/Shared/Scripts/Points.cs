c2e4d6e4129f3059ad402e71496c0df2a4126600
Sir Quarian <sir.quarian@theknightsofunity.com>
2021-05-27 22:16:31 +0200

    Added Points managment

---
+++ Assets/Shared/Scripts/Points.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+
+public static class PointsDatabase
+{
+    public enum Field
+    {
+        Racer, Nonogram, Arcanoid, Frogger
+    }
+
+    public static int SumAllPoints()
+    {
+        int sum = 0;
+        foreach(Field field in Enum.GetValues(typeof(Field)))
+        {
+            sum += Load(field);
+        }
+
+        return sum;
+    }
+
+    public static void Save(Field field, int value)
+    {
+        PlayerPrefs.SetInt(field.ToString(), value);
+        PlayerPrefs.Save();
+    }
+
+    public static int Load(Field field)
+    {
+        return PlayerPrefs.GetInt(field.ToString());
+    }
+}
